6aba901e24fe646a68dd7aea659d3873cdfe0f2a
Fernando Caldera <fcaldera@cebroker.com>
2017-08-16 17:38:55 -0500

    Validate create-react-app instalation

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const spawn = require('cross-spawn');
+const execSync = require('child_process').execSync;
 
 const packageJson = require('./package.json');
 
@@ -14,7 +16,7 @@ let projectTemplate;
 program
   .name('craft')
   .version(packageJson.version)
-  .arguments('<project-directory> <template-url>')
+  .arguments('<project-directory> [template-url]')
   .usage(`${chalk.green('<project-directory>')} ${chalk.cyan('<template-url>')} [options]`)
   .action(function (name, template) {
     projectName = name;
@@ -22,7 +24,7 @@ program
   }).parse(process.argv);
   
 if (typeof projectName === 'undefined' || typeof projectTemplate === 'undefined') {
-  console.error('Please specify the project directory:');
+  console.error('Please specify the project directory and template url:');
   console.log(
     `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')} ${chalk.yellow('<template-url>')}`
   );
@@ -35,5 +37,40 @@ if (typeof projectName === 'undefined' || typeof projectTemplate === 'undefined'
   );
   process.exit(1);
 }
-  
 
+if (isCRAInstalled()) {
+  console.error('No create-react-app instalation has been detected.');
+  console.log('Please install create-react-app to continue.');
+  console.log();
+  console.log(`  ${chalk.cyan('npm')} install -g ${chalk.bold('create-react-app')}`)
+  process.exit(1);
+}
+
+// createApp(projecName);
+
+function createApp(name) {
+  return new Promise((resolve, reject) => {
+    const command = 'create-react-app';
+    const args = [name];
+
+    const child = spawn(command, args, { stdio: 'inherit' });
+    child.on('close', code => {
+      if (code !== 0) {
+        reject({
+          command: `${command} ${args.join(' ')}`,
+        });
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+function isCRAInstalled() {
+  try {
+    execSync('create-react-app --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
